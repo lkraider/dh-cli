7fd8562e316e96d24c06aeb8573dcdc53cc8efac
Paul Eipper <paul@nkey.com.br>
2020-12-15 22:51:44 -0300

    traverse DNS as a filesystem tree

--- dh-cli.py
+++ dh-cli.py
@@ -2,6 +2,7 @@ import cmd
 import json
 import uuid
 import shutil
+import logging
 import argparse
 import dataclasses
 import urllib.parse
@@ -9,7 +10,10 @@ import urllib.request
 
 from dataclasses import dataclass, field
 
-OPTS = {}
+OPTS = {'key', }
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
@@ -48,28 +52,110 @@ class DHMain(DHCmd):
         return DNS().cmdloop()
 
 
+class DNSTree:
+
+    tree: dict = None
+    path: list = None
+
+    def __init__(self, tree=None, path=None):
+        self.tree = tree or {}
+        self.path = path or []
+
+    def keys(self, path: str=None) -> list:
+        return self.view(path).keys()
+
+    def values(self, path: str=None) -> list:
+        return self.view(path).values()
+
+    def items(self, path: str=None) -> list:
+        return self.view(path).items()
+
+    def view(self, path: str=None) -> dict:
+        root = [] if path and path.startswith('/') else self.path
+        subpath = list(filter(None, path.split('/'))) if path else []
+        fullpath = root + subpath
+        return self._recursive_view(self.tree, fullpath)
+
+    @classmethod
+    def _recursive_view(cls, data: dict, path: list, parent: str=None) -> dict:
+        if not isinstance(data, dict):
+            return {parent: None}
+        path = path.copy()
+        key = path.pop(0) if path else None
+        if key is not None:
+            if key not in data:
+                raise DNSTreePathNotFound(key)
+            if not isinstance(data[key], dict) and path:
+                raise DNSTreePathNotFound('/'.join([key] + path))
+            return cls._recursive_view(data[key], path, key)
+        return data
+
+
+class DNSTreePathNotFound(Exception):
+    pass
+
+
 class DNS(DHCmd):
 
-    prompt = 'dh.dns % '
+    _prompt = 'dh.dns {} % '
     _cache = {}
-    _zones = []
+    _tree = DNSTree()
+
+    @property
+    def prompt(self):
+        path = self._tree.path
+        return self._prompt.format(path[-1] if path else '')
+
+    def do_cd(self, arg):
+        if arg == '..':
+            self._tree.path.pop()
+        elif arg in self._tree.view():
+            self._tree.path.append(arg)
+        else:
+            print('cd: no such zone or record: {}'.format(arg))
 
     def do_ls(self, arg):
         self.refresh_records()
-        _print_columns(self._zones)
+        try:
+            keys = sorted(self._tree.keys(arg))
+            _print_columns(keys)
+        except DNSTreePathNotFound as path:
+            print('ls: no such zone or record: {}'.format(path))
 
     def refresh_records(self):
         if self._cache:
             return
         cmd = 'dns-list_records'
         self._cache = _make_request(cmd)
-        self._parse_cache()
+        self.parse_cache()
 
-    def _parse_cache(self):
-        self._zones = sorted(set(k['zone'] for k in self._cache['data']))
+    def parse_cache(self):
+        if self._cache.get('result') != 'success':
+            logger.error('DreamHost API response error')
+            return
+        data = self._cache['data']
+        zones = set(k['zone'] for k in data)
+        records = set((k['zone'], k['record']) for k in data)
+        names = list((k['record'], self._format_name(k), k) for k in data)
+        data = {
+            zone: {
+                record[1]: {name[1]: name[2]
+                for name in names if name[0] == record[1]}
+                for record in records if record[0] == zone}
+                for zone in zones
+        }
+        self._tree.tree = data
+
+    @staticmethod
+    def _format_name(record, limit=15):
+        type_ = record['type']
+        value = record['value'].replace(' ', '_')
+        split = (round((limit-2)/2+0.5), round((limit-2)/2-0.5))
+        value = len(value)>limit and value[:split[0]]+'..'+value[-split[1]:] or value
+        return '{}_{}'.format(type_, value)
 
 
-def _print_columns(values, col_align=8):
+def _print_columns(values: list, col_align: int=8):
     max_width = shutil.get_terminal_size((80, 20)).columns
     value_width = max(len(i) for i in values)
     col_width = value_width + (col_align - value_width % col_align)
@@ -88,9 +174,10 @@ def _make_request(cmd: str, opts: dict=None, **kwargs):
     kwargs.update(api.args_dict())
     url = _build_url(api.url, kwargs)
     req = urllib.request.Request(url)
-    print(req.full_url)
+    logger.debug(req.full_url)
     res = urllib.request.urlopen(req)
     data = json.load(res)
+    logger.debug(data.get('result'))
     return data
 
 
@@ -109,10 +196,12 @@ def _send_request(req) -> str:
 def _make_args():
     parser = argparse.ArgumentParser(description='DreamHost API CLI')
     parser.add_argument('--api-key', dest='key', default='6SHU5P2HLDAYECUM')
+    parser.add_argument('--verbose', '-v', action='store_true')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = _make_args()
-    OPTS = vars(args)
+    OPTS = {k: getattr(args, k) for k in OPTS}
+    logger.level = logging.DEBUG if args.verbose else logging.WARNING
     DHMain().cmdloop()
